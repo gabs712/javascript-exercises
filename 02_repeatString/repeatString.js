bd1b66b19f8ce0de44d3e088c3f53784e3069bcb
gabs <gabs712.g@gmail.com>
2024-01-30 20:41:10 -0300

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+  let alteredString = ''
+  for (let i = 0; i < repeat; i++) {
+    alteredString += string
+  }
+  return alteredString
 };
 
 // Do not edit below this line
