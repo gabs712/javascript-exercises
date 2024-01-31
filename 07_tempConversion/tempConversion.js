4d505019123d4c67dd1ffaca5ea23332338c1800
gabs <gabs712.g@gmail.com>
2024-01-31 00:58:43 -0300

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let formula = ((temp - 32) * 5 / 9)
+  return formula ? +formula.toFixed(1) : 0
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let formula = ((temp * (9 / 5)) + 32)
+  return +formula.toFixed(1) || 0
 };
 
 // Do not edit below this line
