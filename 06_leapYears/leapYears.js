919048c5410343069e98cc31af1875492d16069f
gabs <gabs712.g@gmail.com>
2024-01-31 00:35:28 -0300

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let isLeapYear
 
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) isLeapYear = true
+      else isLeapYear = false
+    } else isLeapYear = true
+  } else isLeapYear = false
+  
+  return isLeapYear
 };
 
 // Do not edit below this line
