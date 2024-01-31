a5dd8443ec66674312554f8a78040ca8868e88a3
gabs <gabs712.g@gmail.com>
2024-01-30 21:22:08 -0300

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  tempString = []
+  for (let char of string) tempString.push(char)
 
-};
+  return tempString.reverse().join('')
+};console.log('heyo'.split(''))
 
 // Do not edit below this line
 module.exports = reverseString;
