528b6147ad0545b3e8a48f482a0a2af9a94eff5d
gabs <gabs712.g@gmail.com>
2024-02-09 20:18:27 -0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let reversed = string.split('').reverse().join('')
 
+  reversed = reversed.toLowerCase().replace(/[ ,.!?]/g, '')
+  string = string.toLowerCase().replace(/[ ,.!?]/g, '')
+
+  return reversed === string
 };
 
 // Do not edit below this line
