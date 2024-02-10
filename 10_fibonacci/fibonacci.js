cb7ec5e0e5a43b3669aabd5978f414b1f053d7b0
gabs <gabs712.g@gmail.com>
2024-02-09 21:57:11 -0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n = 1) {
+  if (n == 0) return 0
+  if (n < 0) return 'OOPS'
 
-};
-
+  let fibonacci = []
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || i === 1) fibonacci.push(1)
+    else  fibonacci.push(fibonacci[fibonacci.length -1] +
+          fibonacci[fibonacci.length -2])
+  }
+  return fibonacci[fibonacci.length - 1]
+}
 // Do not edit below this line
 module.exports = fibonacci;
