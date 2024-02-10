e5e856e2ecb71c30f074df5f802dcdb4e5d7aca5
gabs <gabs712.g@gmail.com>
2024-02-09 23:00:29 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((acc, person) => {
+    const year = new Date().getFullYear()
+    if (Object.keys(acc).length === 0) acc = person
 
+    let personAge = (person.yearOfDeath || year) - person.yearOfBirth
+    let accAge = (acc.yearOfDeath || year) - acc.yearOfBirth
+
+    if (personAge > accAge) acc = person
+    return acc
+  }, {})
 };
 
 // Do not edit below this line
